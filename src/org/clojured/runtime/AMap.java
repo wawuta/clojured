a723d0af735ba855b5d18fb457fae42060602773
wawuta <wawuta@gmail.com>
2014-06-27 20:17:41 +0800

    added AMap,AFn,RT,RestFns

---
+++ src/org/clojured/runtime/AMap.java
@@ -0,0 +1,11 @@
+package org.clojured.runtime;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fd
+ * Date: 14-6-27
+ * Time: 下午7:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class AMap {
+}
---
+++ src/org/clojured/runtime/Cons.java
@@ -0,0 +1,11 @@
+package org.clojured.runtime;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fd
+ * Date: 14-6-27
+ * Time: 上午12:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class Cons {
+}
